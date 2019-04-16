fb21398ef90275b6ab4cdbc03551a0bc0666401c
Jason Cascio <jasoncascio@Jasons-MacBook-Pro.local>
2019-04-16 09:29:57 -0400

    t

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { doGet } from './server/webapp';
 import './es6';
-import UniqueSet from './unique-set';
+import './unique-set';
 
 global.doGet = doGet;
 
@@ -8,5 +8,3 @@ global.sendmail = (email = 'amit@labnol.org') => {
   GmailApp.sendEmail(email, 'It works!', 'Hello Google Apps Script');
 };
 
-const u = new UniqueSet(1, 2, 3, 4, 4, 4, 4);
-Logger.log(JSON.stringify(u));
