c14e6a5f33855a3c22148278a1b4b0ad1b403118
Jason Cascio <jasoncascio@Jasons-MacBook-Pro.local>
2019-04-16 01:50:00 -0400

    t

--- src/es6/arrow-functions.js
+++ src/es6/arrow-functions.js
@@ -13,8 +13,9 @@ Logger.log(`2*10 = ${multiply(2, 10)}`);
 Logger.log(`3*2 = ${multiply(3, 2)}`);
 
 
-var a={
-  b:[1,2,3].forEach((item)=>{
+const a={
+  b:[1,2,3].forEach(item=>{
     Logger.log(this);
   })
 }
+Logger.log(a);
