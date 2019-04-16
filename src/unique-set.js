36aa30582fa1bc208be20db1fa47e1eaf3a83578
Jason Cascio <jasoncascio@Jasons-MacBook-Pro.local>
2019-04-16 08:50:27 -0400

    t

--- src/unique-set.js
+++ src/unique-set.js
@@ -8,105 +8,120 @@
  */
 
 export default class UniqueSet {
-    constructor() {
-        this._strings = {};
-        this._numbers = {};
-        this._dates = {};
-        for (var i in arguments) {
-            this.push(arguments[i]);
-        }
+  constructor() {
+    this._strings = {};
+    this._numbers = {};
+    this._dates = {};
+    for (var i in arguments) {
+      this.push(arguments[i]);
     }
-    push(input) {
-        (toString.call(input) === '[object Array]' ? input : [input]).forEach((item) => {
-            switch (toString.call(item)) {
-                case '[object Number]':
-                    this._numbers[item] = Number;
-                    break;
-                case '[object String]':
-                    this._strings[item] = String;
-                    break;
-                case '[object Date]':
-                    this._dates[item] = Date;
-                    break;
-                default:
-                    throw new TypeError('UniqueSet can only store strings, numbers, dates. Received "' + toString.call(item) + '" at index ' + item);
-            }
-        });
-        return this;
+  }
+  push(input) {
+    (toString.call(input) === '[object Array]' ? input : [input]).forEach(item => {
+      switch (toString.call(item)) {
+        case '[object Number]':
+          this._numbers[item] = Number;
+          break;
+        case '[object String]':
+          this._strings[item] = String;
+          break;
+        case '[object Date]':
+          this._dates[item] = Date;
+          break;
+        default:
+          throw new TypeError(
+            'UniqueSet can only store strings, numbers, dates. Received "' +
+              toString.call(item) +
+              '" at index ' +
+              item
+          );
+      }
+    });
+    return this;
+  }
+  remove(input) {
+    (toString.call(input) === '[object Array]' ? input : [input]).forEach(item => {
+      switch (toString.call(item)) {
+        case '[object Number]':
+          delete this._numbers[item];
+          break;
+        case '[object String]':
+          delete this._strings[item];
+          break;
+        case '[object Date]':
+          delete this._dates[item];
+          break;
+        default:
+          throw new TypeError(
+            'UniqueSet can only store strings, numbers, dates. Received "' +
+              toString.call(item) +
+              '"'
+          );
+      }
+    });
+    return this;
+  }
+  flush() {
+    this._strings = {};
+    this._numbers = {};
+    this._dates = {};
+    return this;
+  }
+  copyItems(uniqueSet) {
+    if (input instanceof UniqueSet) {
+      return this.flush().push(uniqueSet.values);
+    } else {
+      throw new TypeError('copyIndices accepts only UniqueSet input.');
     }
-    remove(input) {
-        (toString.call(input) === '[object Array]' ? input : [input]).forEach((item) => {
-            switch (toString.call(item)) {
-                case '[object Number]':
-                    delete this._numbers[item];
-                    break;
-                case '[object String]':
-                    delete this._strings[item];
-                    break;
-                case '[object Date]':
-                    delete this._dates[item];
-                    break;
-                default:
-                    throw new TypeError('UniqueSet can only store strings, numbers, dates. Received "' + toString.call(item) + '"');
-            }
-        });
-        return this;
+  }
+  get values() {
+    return []
+      .concat(
+        Object.keys(this._numbers).map(item => {
+          return this._numbers[item](item);
+        })
+      )
+      .concat(
+        Object.keys(this._strings).map(item => {
+          return this._strings[item](item);
+        })
+      )
+      .concat(
+        Object.keys(this._dates).map(item => {
+          return this._dates[item](item);
+        })
+      );
+  }
+  get contains() {
+    return {
+      numbers: Object.keys(this._numbers).length,
+      strings: Object.keys(this._strings).length,
+      dates: Object.keys(this._dates).length
+    };
+  }
+  get holds() {
+    var returnArray = [];
+    var contains = this.contains;
+    if (contains.numbers) {
+      returnArray.push('numbers');
     }
-    flush() {
-        this._strings = {};
-        this._numbers = {};
-        this._dates = {};
-        return this;
+    if (contains.strings) {
+      returnArray.push('strings');
     }
-    copyItems(uniqueSet) {
-        if (input instanceof UniqueSet) {
-            return this.flush().push(uniqueSet.values);
-        } else {
-            throw new TypeError('copyIndices accepts only UniqueSet input.');
-        }
+    if (contains.dates) {
+      returnArray.push('dates');
     }
-    get values() {
-        return []
-            .concat(Object.keys(this._numbers).map((item) => {
-                return this._numbers[item](item);
-            }))
-            .concat(Object.keys(this._strings).map((item) => {
-                return this._strings[item](item);
-            }))
-            .concat(Object.keys(this._dates).map((item) => {
-                return this._dates[item](item);
-            }));
-    }
-    get contains() {
-        return {
-            numbers: Object.keys(this._numbers).length,
-            strings: Object.keys(this._strings).length,
-            dates: Object.keys(this._dates).length
-        }
-    }
-    get holds() {
-        var returnArray = [];
-        var contains = this.contains;
-        if (contains.numbers) {
-            returnArray.push('numbers')
-        };
-        if (contains.strings) {
-            returnArray.push('strings')
-        };
-        if (contains.dates) {
-            returnArray.push('dates')
-        };
-        return returnArray;
-    }
-    get pure() {
-        return this.holds.length < 2;
-    }
-    get isEmpty() {
-        return this.values.length === 0;
-    }
-    get length() {
-        return this.values.length;
-    }
+    return returnArray;
+  }
+  get pure() {
+    return this.holds.length < 2;
+  }
+  get isEmpty() {
+    return this.values.length === 0;
+  }
+  get length() {
+    return this.values.length;
+  }
 }
 
 const u = new UniqueSet(1, 2, 3, 4, 5, 5, 5, 5, '1');
