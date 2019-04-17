a52c592f31ac545df6821f789b986b01d177403e
Jason Cascio <jasoncascio@Jasons-MBP.routerlogin.net>
2019-04-16 22:10:30 -0400

    t

--- src/simple-clone.js
+++ src/simple-clone.js
@@ -3,38 +3,38 @@
  * @desc taken from https://stackoverflow.com/questions/728360/how-do-i-correctly-clone-a-javascript-object
  */
 
-export default simpleClone = input => {
-    let copy;
-    const toStringType=toString.call(input);
-    switch (toStringType) {
-      case "[object Undefined]":
-      case "[object Null]":
-      case "[object Number]":
-      case "[object String]":
-        copy = input;
-        break;
-      case "[object Array]":
-        copy = input.map(i => {
-          return simpleClone(i);
-        });
-        break;
-      case "[object Object]":
-        copy = {};
-        for (var property in input) {
-          if (input.hasOwnProperty(property)) {
-            copy[property] = simpleClone(input[property]);
-          }
+export const simpleClone = (input) => {
+  let copy;
+  const toStringType=toString.call(input);
+  switch (toStringType) {
+    case "[object Undefined]":
+    case "[object Null]":
+    case "[object Number]":
+    case "[object String]":
+      copy = input;
+      break;
+    case "[object Array]":
+      copy = input.map(i => {
+        return simpleClone(i);
+      });
+      break;
+    case "[object Object]":
+      copy = {};
+      for (var property in input) {
+        if (input.hasOwnProperty(property)) {
+          copy[property] = simpleClone(input[property]);
         }
-        break;
-      case "[object Date]":
-        copy = new Date();
-        copy.setTime(input.getTime());
-        break;
-      default:
-        throw new TypeError(
-          `Unable to clone: object type ${toStringType} is unsupported.`
-        );
-    }
-    return copy;
-  };
+      }
+      break;
+    case "[object Date]":
+      copy = new Date();
+      copy.setTime(input.getTime());
+      break;
+    default:
+      throw new TypeError(
+        `Unable to clone: object type ${toStringType} is unsupported.`
+      );
+  }
+  return copy;
+};
   
