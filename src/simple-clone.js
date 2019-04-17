671e00164fd0bb138f2dd36f1cbf6f0d37f1e435
Jason Cascio <jasoncascio@Jasons-MBP.routerlogin.net>
2019-04-16 22:12:46 -0400

    t

--- src/simple-clone.js
+++ src/simple-clone.js
@@ -7,33 +7,31 @@ export const simpleClone = (input) => {
   let copy;
   const toStringType=toString.call(input);
   switch (toStringType) {
-    case "[object Undefined]":
-    case "[object Null]":
-    case "[object Number]":
-    case "[object String]":
+    case '[object Undefined]':
+    case '[object Null]':
+    case '[object Number]':
+    case '[object String]':
       copy = input;
       break;
-    case "[object Array]":
+    case '[object Array]':
       copy = input.map(i => {
         return simpleClone(i);
       });
       break;
-    case "[object Object]":
+    case '[object Object]':
       copy = {};
-      for (var property in input) {
+      for (let property in input) {
         if (input.hasOwnProperty(property)) {
           copy[property] = simpleClone(input[property]);
         }
       }
       break;
-    case "[object Date]":
+    case '[object Date]':
       copy = new Date();
       copy.setTime(input.getTime());
       break;
     default:
-      throw new TypeError(
-        `Unable to clone: object type ${toStringType} is unsupported.`
-      );
+      throw new TypeError(`Unable to clone: object type ${toStringType} is unsupported.`);
   }
   return copy;
 };
