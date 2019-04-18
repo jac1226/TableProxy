7f53cc7d2d1adce54387233c72aedbf5438e32d8
Jason Cascio <jasoncascio@Jasons-MacBook-Pro.local>
2019-04-18 00:38:16 -0400

    t

--- src/index.js
+++ src/index.js
@@ -3,24 +3,28 @@ import SheetAccessor from './sheet-accessor';
 import QueryDriver from './query-driver';
 import QueryReturn from './query-return';
 
-const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Test');
-const sheetAccessor = new SheetAccessor(sheet);
+const doGet = () => {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Test');
+  const sheetAccessor = new SheetAccessor(sheet);
+  const query = r => {
+    let balls = 1;
+    if (r.nuggets.value === r.nuggets.fontcolor) {
+      balls = 5;
+    }
+    return balls;
+  };
+  const queryDriver = new QueryDriver(query, 'test');
+  const queryReturn = new QueryReturn(queryDriver);
 
-const query = r => {
-  let balls = 1;
-  if (r.nuggets.value === r.nuggets.fontcolor) {
-    balls = 5;
-  }
-  return balls;
-};
+  const output={
+    sheet:sheet,
+    sheetAccessor:sheetAccessor,
+    queryDriver:queryDriver,
+    queryReturn:queryReturn
+  };
 
-const queryDriver = new QueryDriver(query, 'test');
-const queryReturn = new QueryReturn(queryDriver);
-
-function test() {
-  Logger.log(JSON.stringify(sheet));
-  Logger.log(JSON.stringify(sheetAccessor));
-  Logger.log(JSON.stringify(queryDriver));
-  Logger.log(JSON.stringify(queryReturn));
+  return ContentService.createTextOutput(output)
+  .setMimeType(ContentService.MimeType.JSON);
 }
-test();
+
+global.doGet = doGet;
