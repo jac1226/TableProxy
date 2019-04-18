853512e2dc7a5c86b344dfdc1e7b5330e6a5c034
Jason Cascio <jasoncascio@Jasons-MacBook-Pro.local>
2019-04-17 22:45:09 -0400

    t

--- src/query-return.js
+++ src/query-return.js
@@ -1,38 +1,38 @@
 /**
-* Query Return Object
-* @return {Object}
-*/
+ * Query Return Object
+ * @return {Object}
+ */
 
 import { getTimeStamp, getTimeDiff } from './utilities';
 import UniqueSet from './unique-set';
-import QueryDriver from './query-driver'
+import QueryDriver from './query-driver';
 
 export default class QueryReturn {
-    constructor(queryDriver){
-        if (!(queryDriver instanceof QueryDriver)) {
-            throw new TypeError('QueryResult constructor requires QueryDriver input.');
-        }
-        this.query = queryDriver.query;
-        this.type = queryDriver.type;
-        this.resultSet = new UniqueSet();
-        this.queryStartTime = getTimeStamp();
-        this.queryDuration = null;
-        this.returnContainer = {
-            records: {},
-            errors: []
-        };
+  constructor(queryDriver){
+    if (!(queryDriver instanceof QueryDriver)) {
+      throw new TypeError('QueryResult constructor requires QueryDriver input.');
     }
+    this.query = queryDriver.query;
+    this.type = queryDriver.type;
+    this.resultSet = new UniqueSet();
+    this.queryStartTime = getTimeStamp();
+    this.queryDuration = null;
+    this.returnContainer = {
+        records: {},
+        errors: []
+    };
+  }
 
-    get count(){
-        return this.resultSet.length;
-    }
+  get count(){
+    return this.resultSet.length;
+  }
 
-    push(input){
-        this.resultSet.push(input);
-    }
+  push(input){
+    this.resultSet.push(input);
+  }
 
-    done(){
-        this.queryDuration = getTimeDiff(this.queryStartTime);
-        Logger.log(this.type + ' operation completed in ' + this.queryDuration + 'ms.\n' + this.query.toString());
-    }
+  done(){
+    this.queryDuration = getTimeDiff(this.queryStartTime);
+    Logger.log(this.type + ' operation completed in ' + this.queryDuration + 'ms.\n' + this.query.toString());
+  }
 }
